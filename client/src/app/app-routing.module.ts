57ff796ad5f88cf118d2bc557619685c62b3746b
Nate <n8blake@mac.com>
2022-05-04 13:13:37 -0400

    Add routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch:'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { NavComponent } from './common/nav/nav.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
